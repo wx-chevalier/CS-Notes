3b092959bc2650440162a8645e3a03746e3dfce9
wxyyxc1992 <hoteam@live-forest.com>
2016-08-01 20:27:16 +0800

    添加类的初始化相关代码

---
+++ code/src/main/java/wx/toolkits/jvm/clasz/loading/Child.java
@@ -0,0 +1,25 @@
+package wx.toolkits.jvm.clasz.loading;
+
+/**
+ * Created by apple on 16/8/1.
+ */
+public class Child extends Parent {
+
+    static {
+        System.out.println("子类静态初始化");
+    }
+
+    {
+        System.out.println("子类实例初始化");
+    }
+
+    Child() {
+        System.out.println("子类构造函数");
+    }
+
+    public static void main(String args[]) {
+
+        Child child = new Child();
+
+    }
+}
---
+++ code/src/main/java/wx/toolkits/jvm/clasz/loading/PassiveAccessTest.java
@@ -0,0 +1,12 @@
+package wx.toolkits.jvm.clasz.loading;
+
+/**
+ * Created by apple on 16/8/1.
+ */
+public class PassiveAccessTest {
+
+    public static void main(String[] args) {
+        System.out.println(Child.language);
+    }
+
+}
