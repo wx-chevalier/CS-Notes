7956bb9b7a92c510bac32f05d18f80640aaae9e7
wxyyxc1992 <hoteam@live-forest.com>
2016-09-07 13:49:07 +0800

    添加部分Java测试代码

--- code/src/main/java/wx/toolkits/ds/string/StringGenerator.java
+++ code/src/main/java/wx/toolkits/ds/string/StringGenerator.java
@@ -1,4 +1,7 @@
-package wx.ds.string;
+package wx.toolkits.ds.string;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.Random;
 
@@ -7,7 +10,12 @@ import java.util.Random;
  */
 public class StringGenerator {
 
-    public static String getRandomString(int length) { //length表示生成字符串的长度
+    /**
+     * @param length 字符串长度
+     * @return 新的随机生成的字符串
+     * @function 生成固定长度的字符串
+     */
+    public static String generateFixedLengthString(int length) { //length表示生成字符串的长度
         String base = "abcdefghijklmnopqrstuvwxyz0123456789";
         Random random = new Random();
         StringBuffer sb = new StringBuffer();
@@ -18,4 +26,33 @@ public class StringGenerator {
         return sb.toString();
     }
 
+    @Test
+    public void test_generateFixedLengthString() { //length表示生成字符串的长度
+        System.out.println(StringGenerator.generateFixedLengthString(5));
+    }
+
+    /**
+     * @param originStr 原始的字符串
+     * @return
+     * @function 反转字符串
+     */
+    public static String reverseString(String originStr) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = originStr.length() - 1; i > -1; i--) {
+
+            stringBuilder.append(originStr.charAt(i));
+        }
+
+        return stringBuilder.toString();
+
+    }
+
+    @Test
+    public void test_reverseString() {
+
+        Assert.assertEquals("bacd", reverseString("dcab"));
+
+    }
 }
